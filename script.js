bfb556d519f207194f7079b3751940a63eb84c52
Dimithri8 <dimithridananjaya924@gmail.com>
2025-08-24 15:14:03 +0530

    Added logic to render all movies for a given search term

--- script.js
+++ script.js
@@ -7,54 +7,60 @@ const searchBtn = document.querySelector(".search-btn");
 searchBtn.addEventListener("click", () => {
   const userInput = searchBar.value;
   getMovie(userInput);
+
+  movieResultsContaner.innerHTML = "";
 });
 
 async function getMovie(movieName) {
   try {
     const res = await fetch(
-      `http://www.omdbapi.com/?t=${movieName}&apikey=59ccd4a7`
+      `http://www.omdbapi.com/?s=${encodeURIComponent(
+        movieName
+      )}&apikey=59ccd4a7`
     );
     if (!res.ok) throw Error("Movie cannot be found.");
     const movie = await res.json();
     if (movie.Response === "False") throw Error("Movie not found");
+    console.log(movie);
+    movie.Search.forEach((movieItem) => {
+      const movieCard = document.createElement("div");
+      movieCard.classList.add("movie-card");
+      movieResultsContaner.appendChild(movieCard);
 
-    const movieCard = document.createElement("div");
-    movieCard.classList.add("movie-card");
-    movieResultsContaner.appendChild(movieCard);
+      const moviePoster = document.createElement("img");
+      moviePoster.setAttribute("src", movieItem.Poster);
+      moviePoster.classList.add("movie-poster");
 
-    const moviePoster = document.createElement("img");
-    moviePoster.setAttribute("src", movie.Poster);
-    moviePoster.classList.add("movie-poster");
+      const movieTitle = document.createElement("h2");
+      movieTitle.textContent = movieItem.Title;
+      movieTitle.classList.add("movie-title");
 
-    const movieTitle = document.createElement("h2");
-    movieTitle.textContent = movie.Title;
-    movieTitle.classList.add("movie-title");
+      const movieYear = document.createElement("p");
+      movieYear.textContent = movieItem.Year;
+      movieYear.classList.add("movie-year");
 
-    const movieYear = document.createElement("p");
-    movieYear.textContent = movie.Year;
-    movieYear.classList.add("movie-year");
+      const movieRating = document.createElement("p");
+      movieRating.textContent = movieItem.imdbRating;
+      movieRating.classList.add("movie-year");
 
-    const movieRating = document.createElement("p");
-    movieRating.textContent = movie.imdbRating;
-    movieRating.classList.add("movie-year");
+      const moviePlot = document.createElement("p");
+      moviePlot.textContent = movieItem.Plot;
+      moviePlot.classList.add("movie-plot");
 
-    const moviePlot = document.createElement("p");
-    moviePlot.textContent = movie.Plot;
-    moviePlot.classList.add("movie-plot");
+      const posterContainer = document.createElement("div");
+      const movieTextContainer = document.createElement("div");
+      movieTextContainer.classList.add("movieTextContainer");
 
-    const posterContainer = document.createElement("div");
-    const movieTextContainer = document.createElement("div");
-    movieTextContainer.classList.add("movieTextContainer");
+      movieCard.appendChild(posterContainer);
+      posterContainer.classList.add("poster-container");
+      posterContainer.appendChild(moviePoster);
 
-    movieCard.appendChild(posterContainer);
-    posterContainer.classList.add("poster-container");
-    posterContainer.appendChild(moviePoster);
-
-    movieCard.appendChild(movieTextContainer);
-    movieTextContainer.appendChild(movieTitle);
-    movieTextContainer.appendChild(movieYear);
-    movieTextContainer.appendChild(movieRating);
-    movieTextContainer.appendChild(moviePlot);
+      movieCard.appendChild(movieTextContainer);
+      movieTextContainer.appendChild(movieTitle);
+      movieTextContainer.appendChild(movieYear);
+      movieTextContainer.appendChild(movieRating);
+      movieTextContainer.appendChild(moviePlot);
+    });
   } catch (error) {
     console.error(error);
   } finally {
